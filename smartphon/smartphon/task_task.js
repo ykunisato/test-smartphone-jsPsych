8b62d927c57acd33bc890c8361499f45d202dab0
ykunisato <ykunisato@psy.senshu-u.ac.jp>
2023-02-08 21:21:20 +0000

    update

---
+++ smartphon/smartphon/task_task.js
@@ -0,0 +1,8 @@
+/* 課題に関するコードを以下に書く */
+const welcome = {
+  type: jsPsychHtmlKeyboardResponse,
+  stimulus: "こんにちは！ この課題は何かキーボードのキーをタイプするとデータを保存して終わります。"
+};
+
+/*タイムラインの設定*/
+const timeline = [fullscreen,welcome];
