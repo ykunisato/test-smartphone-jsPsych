a927b00996784709ce202c9a38b854282bce9612
ykunisato <ykunisato@psy.senshu-u.ac.jp>
2023-02-09 01:54:04 +0000

    multi-choice

--- smartphon/smartphon/task_questionnaire.js
+++ smartphon/smartphon/task_questionnaire.js
@@ -1,29 +1,34 @@
-/* 課題に関するコードを以下に書く */
-const scale = {
-    '全く違うと思う': 1,
-    'おおよそ違うと思う': 2,
-    '少し違うと思う': 3,
-    'どちらでもない': 4,
-    '少しそう思う': 5,
-    'まあまあそう思う': 6,
-    '強くそう思う': 7,
-  };
-/* 質問紙の設定 */
-const likert_page = {
-    type: jsPsychSurveyMatrixLikert,
-    questions: [
-      {prompt: '心理学のことが好きだ', required: true, name: '好き', reverse_item: false},
-      {prompt: '心理学のことを一日中考えている', required: true, name: '考える', reverse_item: true},
-      {prompt: '心理学のことを悪く言われると気分が悪い', required: true, name: '気分悪い'},
-      {prompt: '私は心理学のために尽くすタイプだ', required: false, name: '尽くす'}
-    ],
-    randomize_question_order: false,
-    scale: scale,
-    column_header_alignment: 'bottom', // 'top': 上揃い, 'center': 中揃い, 'bottom': 下揃い
-    preamble: 'この質問紙はあなたの心理学に対する感情を測定するものです。<br> 以下の項目をよく読んで，あなた自身にどのくらい当てはまるか，当てはまるものをクリックしてください。',
-    cellpadding: 16,
-    button_label: '次へ',
-    autocomplete: true
-  };
+var trial = {
+  type: jsPsychSurvey,
+  pages: [
+    [
+      {
+        type: 'html',
+        prompt: 'この質問紙はあなたの心理学に対する感情を測定するものです。<br> 以下の項目をよく読んで，あなた自身にどのくらい当てはまるか，当てはまるものを選んでください',
+      },
+      {
+        type: 'multi-choice',
+        prompt: "心理学のことが好きだ", 
+        name: 'VegetablesLike', 
+        options: ['全く違うと思う','おおよそ違うと思う','少し違うと思う','どちらでもない','少しそう思う','まあまあそう思う','強くそう思う'], 
+        required: true
+      }, 
+      {
+        type: 'multi-choice',
+        prompt: "心理学のことを一日中考えている", 
+        name: 'FruitLike', 
+        options: ['全く違うと思う','おおよそ違うと思う','少し違うと思う','どちらでもない','少しそう思う','まあまあそう思う','強くそう思う'],  
+        required: false,
+      }, 
+      {
+        type: 'multi-choice',
+        prompt: "私は心理学のために尽くすタイプだ", 
+        name: 'FruitLike', 
+        options: ['全く違うと思う','おおよそ違うと思う','少し違うと思う','どちらでもない','少しそう思う','まあまあそう思う','強くそう思う'],  
+        required: false,
+      }
+    ]
+  ],
+};
 /*タイムラインの設定*/
-const timeline = [likert_page];
+const timeline = [trial]
