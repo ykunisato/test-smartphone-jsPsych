f43729cbf7021cadabef415deca0d2a64b27c19a
ykunisato <ykunisato@psy.senshu-u.ac.jp>
2023-02-09 01:40:21 +0000

    update

--- smartphon/smartphon/task_questionnaire.js
+++ smartphon/smartphon/task_questionnaire.js
@@ -1,63 +1,29 @@
 /* 課題に関するコードを以下に書く */
-const trial = {
-  type: jsPsychSurvey,
-  pages: [
-    [
-      {
-        type: 'likert',
-        prompt: 'I like to eat vegetables.',
-        likert_scale_min_label: '全く同意しない',
-        likert_scale_max_label: '強く同意する',
-        likert_scale_values: [
-          {value: 1},
-          {value: 2},
-          {value: 3},
-          {value: 4},
-          {value: 5}
-        ]
-      }, 
-      {
-        type: 'likert',
-        prompt: 'I like to eat fruit.',
-        likert_scale_min_label: 'Strongly Disagree',
-        likert_scale_max_label: 'Strongly Agree',
-        likert_scale_values: [
-          {value: 1},
-          {value: 2},
-          {value: 3},
-          {value: 4},
-          {value: 5}
-        ]
-      },
-      {
-        type: 'likert',
-        prompt: 'I like to eat meat.',
-        likert_scale_min_label: 'Strongly Disagree',
-        likert_scale_max_label: 'Strongly Agree',
-        likert_scale_values: [
-          {value: 1},
-          {value: 2},
-          {value: 3},
-          {value: 4},
-          {value: 5}
-        ]
-      },  
-
+const scale = {
+    '全く違うと思う': 1,
+    'おおよそ違うと思う': 2,
+    '少し違うと思う': 3,
+    'どちらでもない': 4,
+    '少しそう思う': 5,
+    'まあまあそう思う': 6,
+    '強くそう思う': 7,
+  };
+/* 質問紙の設定 */
+const likert_page = {
+    type: jsPsychSurveyMatrixLikert,
+    questions: [
+      {prompt: '心理学のことが好きだ', required: true, name: '好き', reverse_item: false},
+      {prompt: '心理学のことを一日中考えている', required: true, name: '考える', reverse_item: true},
+      {prompt: '心理学のことを悪く言われると気分が悪い', required: true, name: '気分悪い'},
+      {prompt: '私は心理学のために尽くすタイプだ', required: false, name: '尽くす'}
     ],
-    [
-      {
-        type: 'likert-table',
-        prompt: ' ',
-        statements: [
-          {prompt: 'I like to eat vegetables', name: 'VeggiesTable'},
-          {prompt: 'I like to eat fruit', name: 'FruitTable'},
-          {prompt: 'I like to eat meat', name: 'MeatTable'},
-        ],
-        options: ['Strongly Disagree', 'Disagree', 'Neutral', 'Agree', 'Strongly Agree'],
-      }
-    ]
-  ],
-};
-
+    randomize_question_order: false,
+    scale: scale,
+    column_header_alignment: 'bottom', // 'top': 上揃い, 'center': 中揃い, 'bottom': 下揃い
+    preamble: 'この質問紙はあなたの心理学に対する感情を測定するものです。<br> 以下の項目をよく読んで，あなた自身にどのくらい当てはまるか，当てはまるものをクリックしてください。',
+    cellpadding: 16,
+    button_label: '次へ',
+    autocomplete: true
+  };
 /*タイムラインの設定*/
-const timeline = [trial];
+const timeline = [likert_page];
