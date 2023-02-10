f969778deb235f6d4b40222637e97c30580b203b
ykunisato <ykunisato@psy.senshu-u.ac.jp>
2023-02-10 02:51:15 +0000

    update

--- smartphon/smartphon/task_task.js
+++ smartphon/smartphon/task_task.js
@@ -1,5 +1,13 @@
 /* 課題に関するコードを以下に書く */
-const instruction = {
+
+const instruction1 = {
+  type: jsPsychHtmlButtonResponse,
+  stimulus: '<p style="text-align: left">これから行う課題は，スマートホンを横向きにして行います。</p>' + '<p style="text-align: left">イラストの様にスマートホンを横向きにしてください</p>' +'<p style="text-align: left"></p>' + '<p > <div style="position: fixed; top: 50%; left: 50%"><img src="smartphon/stimuli/smartphone_yoko.png" width="20%"/></div></p>',
+  choices: ['スマートホンを横向にしたので次へ進む'],
+  prompt: ""
+};
+
+const instruction2 = {
   type: jsPsychHtmlButtonResponse,
   stimulus: '<p style="text-align: left"> これから画面に２つのスロットマシーンが出てきますので，どちらかを選んでください。</p>' + '<p style="text-align: left">スロットマシーンを選択すると，賞金が得るか失います（なお賞金に羽が生えた絵は賞金を失ったことを意味します）。</p>' + '<p style="text-align: left"> スロットマシーンはどちらかが賞金が出やすいように設定されているので，より多くの賞金を得られる方を選択してください。</p>',
   choices: ['はじめる'],
@@ -20,13 +28,13 @@ const choice = {
       var last_select = jsPsych.data.get().last(1).values()[0].response;
       var fb = jsPsych.timelineVariable('fb_slot1');
       if (last_select==0 && fb==1){
-        return ["<p style='position: absolute; top: 40%;left: 20%'><img src='smartphon/stimuli/gain.gif' width='50%'/></p>"]
+        return ["<p style='position: fixed; top: 40%;left: 20%'><img src='smartphon/stimuli/gain.gif' width='30%'/></p>"]
       } else if (last_select==0 && fb==0){
-        return ["<p style='position: absolute; top: 40%;left: 20%'><img src='smartphon/stimuli/loss.gif' width='50%'/></p>"]
+        return ["<p style='position: fixed; top: 40%;left: 20%'><img src='smartphon/stimuli/loss.gif' width='30%'/></p>"]
       } else if (last_select==1 && fb==0){
-        return ["<p style='position: absolute; top: 40%;left: 20%'><img src='smartphon/stimuli/gain.gif' width='50%'/></p>"]
+        return ["<p style='position: fixed; top: 40%;left: 20%'><img src='smartphon/stimuli/gain.gif' width='30%'/></p>"]
       } else if (last_select==1 && fb==1){
-        return ["<p style='position: absolute; top: 40%;left: 20%'><img src='smartphon/stimuli/loss.gif' width='50%'/></p>"]
+        return ["<p style='position: fixed; top: 40%;left: 20%'><img src='smartphon/stimuli/loss.gif' width='30%'/></p>"]
       }
     },
     button_html: '%choice%',
@@ -57,4 +65,4 @@ const instruction_end = {
 };
 
 /*タイムラインの設定*/
-const timeline = [instruction,choice, instruction_end];
+const timeline = [instruction1,instruction2,choice, instruction_end];
