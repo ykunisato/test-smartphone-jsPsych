b8549eebaa775b0b66688633579670b6c25aae92
ykunisato <ykunisato@psy.senshu-u.ac.jp>
2023-02-10 02:41:46 +0000

    update

--- smartphon/smartphon/task_task.js
+++ smartphon/smartphon/task_task.js
@@ -10,8 +10,8 @@ const choice = {
   timeline:[{
     type: jsPsychHtmlButtonResponse,
     stimulus: '',
-    choices: ['<div style="position: absolute; top: 50%; left: 0%"><img src="smartphon/stimuli/slot1.png" width="20%"/></div>',
-    '<div style="position: absolute; top: 50%; left: 50%"><img src="smartphon/stimuli/slot2.png" width="20%"/></div>'],
+    choices: ['<div style="position: fixed; top: 50%; left: 0%"><img src="smartphon/stimuli/slot1.png" width="20%"/></div>',
+    '<div style="position: fixed; top: 50%; right: 0%"><img src="smartphon/stimuli/slot2.png" width="20%"/></div>'],
     button_html: '%choice%'
   },{
     type: jsPsychHtmlButtonResponse,
