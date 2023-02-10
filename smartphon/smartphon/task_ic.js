cdf65dfd613f835fdd2d25076256bc84c339d1e7
ykunisato <ykunisato@psy.senshu-u.ac.jp>
2023-02-10 01:08:46 +0000

    update

--- smartphon/smartphon/task_ic.js
+++ smartphon/smartphon/task_ic.js
@@ -1,8 +1,10 @@
 /* 課題に関するコードを以下に書く */
-const welcome = {
-  type: jsPsychHtmlKeyboardResponse,
-  stimulus: "こんにちは！ この課題は何かキーボードのキーをタイプするとデータを保存して終わります。"
+var trial = {
+  type: jsPsychHtmlButtonResponse,
+  stimulus: '<p style="font-size:48px; color:red;">GREEN</p>',
+  choices: ['始める'],
+  prompt: "<p>What color is the ink?</p>"
 };
 
 /*タイムラインの設定*/
-const timeline = [fullscreen,welcome];
+const timeline = [welcome];
--- smartphon/smartphon/task_task.js
+++ smartphon/smartphon/task_task.js
@@ -1,8 +1,18 @@
 /* 課題に関するコードを以下に書く */
-const welcome = {
-  type: jsPsychHtmlKeyboardResponse,
-  stimulus: "こんにちは！ この課題は何かキーボードのキーをタイプするとデータを保存して終わります。"
+const instruction = {
+  type: jsPsychHtmlButtonResponse,
+  stimulus: '<p style="text-align: left"> これから画面に２つのスロットマシーンが出てきますので，どちらかを選んでください。</p>' + '<p style="text-align: left">スロットマシーンを選択すると，賞金が得るか失います（なお賞金に羽が生えた絵は賞金を失ったことを意味します）。</p>' + '<p style="text-align: left"> スロットマシーンはどちらかが賞金が出やすいように設定されているので，より多くの賞金を得られる方を選択してください。</p>',
+  choices: ['はじめる'],
+  prompt: ""
+};
+
+const trial = {
+  type: jsPsychHtmlButtonResponse,
+  stimulus: '',
+  choices: ['<div style="position: absolute; top: 50%; left: 20%"><img src="smartphon/stimuli/slot1.png" width="200px"/></div>',
+    '<div style="position: absolute; top: 50%; right: 20%"><img src="smartphon/stimuli/slot2.png" width="200px"/></div>'],
+    button_html: '%choice%'
 };
 
 /*タイムラインの設定*/
-const timeline = [fullscreen,welcome];
+const timeline = [instruction,trial];
